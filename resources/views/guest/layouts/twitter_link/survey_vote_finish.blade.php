816a7bdec97fa7b5eda9bfcb9ebb5d5217fcabb3
DaichiNiwa <daichi.niwa.100@gmail.com>
2020-05-08 19:36:30 +0900

    本番環境アップに向けてマイグレーションファイルを修正。constも少し修正。

--- resources/views/guest/layouts/twitter_link/survey_vote_finish.blade.php
+++ resources/views/guest/layouts/twitter_link/survey_vote_finish.blade.php
@@ -1,7 +1,7 @@
 <a href="https://twitter.com/intent/tweet
 ?text=アンケートに投票しました！%0a%0a
 【{{ $survey->question }}】%0a%0a
-&url=https%3A%2F%2F{{ config('const.URL') . '%2Fsurveys%2F' . $survey->id }}
+&url=https%3A%2F%2F{{ config('app.url') . '%2Fsurveys%2F' . $survey->id }}
 &hashtags=Jリーグ投票"
 class="tweet-btn">
     <i class="fab fa-twitter mr-1"></i><span>投票をTweet</span>
